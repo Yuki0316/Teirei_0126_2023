b92adf8e54fac244fbca97450d30574aa4a63c84
macbook <macbook@MacBook-Air-5.local>
2023-01-28 14:03:16 +0900

    ListTileSwitchをつけた

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:list_tile_switch/list_tile_switch.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final List<String> contents = <String>[
-      'Prevent Cross-Site Tracking',
-      'Hide IP Address',
-      'Block All Cookies',
-      'Fraudulent Website Warning',
-      'Privacy Preserving Ad Measurement',
-      'Check for Apple Pay'
-    ];
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  final List<bool> _switchValues = List.generate(7, (_) => false);
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color(0xffF2F1F7),
         appBar: AppBar(
-          leadingWidth: 120,
+          leadingWidth: 110,
           leading: TextButton.icon(
             onPressed: () {},
             icon: const Icon(Icons.chevron_left),
             label: const Text('Settings'),
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all(EdgeInsets.zero),
+              minimumSize: MaterialStateProperty.all(Size.zero),
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
           ),
           backgroundColor: Colors.white,
           title: const Text(
@@ -37,39 +41,30 @@ class MyApp extends StatelessWidget {
         ),
         body: Stack(
           children: <Widget>[
-            Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                width: 390,
-                height: 270,
-                margin:
-                    const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10)),
-                child: ListView.separated(
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: contents.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 28,
-                            alignment: Alignment.centerLeft,
-                            child: Text(contents[index]),
-                          ),
-                        ]);
-                  },
-                  separatorBuilder: (BuildContext context, int index) =>
-                      const Divider(),
+            Container(
+              width: 390,
+              height: 270,
+              margin: const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              child: ListTileSwitch(
+                value: _switchValues[0],
+                onChanged: (value) {
+                  setState(() {
+                    _switchValues[0] = value;
+                  });
+                },
+                switchActiveColor: const Color(0xff34C759),
+                title: const Text(
+                  'Prevent Cross-Site Tracking',
+                  style: TextStyle(fontSize: 15, color: Colors.black),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
     );
   }
 }
+//Providerを下につけたいのだが、too many positionalを吐かれてなかなかうまくいかない
